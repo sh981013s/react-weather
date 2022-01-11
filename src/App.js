2cb0c18824f5d53ed42bb4a7160dc97bd1b0dfce
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-11 23:26:57 +0900

    Feat: style-reset via GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Reset } from 'styled-reset';
 import { ThemeProvider } from 'styled-components';
 import { Home } from './pages/index';
+import GlobalStyle from './globalStyles';
 import { lightTheme } from './utility/Theme';
 
 const App = () => {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <Router>
-        <Route exact path="/" component={Home} />
-      </Router>
-    </ThemeProvider>
+    <Fragment>
+      <GlobalStyle />
+      <ThemeProvider theme={lightTheme}>
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
+      </ThemeProvider>
+    </Fragment>
   );
 };
 
---
+++ src/globalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset'; // style-reset 패키지
+
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    *,*::before,*::after,h1,h2,h3,h4,h5,h6 {
+      margin: 0;
+      padding: 0;
+    }
+    h1,h2,h3,h4,h5,h6 {
+      display: inline-block;
+    }
+    body {
+      margin: 0;
+      padding: 0;
+      overflow-x: hidden;
+      font-family: 'Source Sans Pro', sans-serif;
+    }
+`;
+
+export default GlobalStyle;
