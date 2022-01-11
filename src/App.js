161db2f786d0b59e25a93a6a826d54ac5d7548e2
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-12 00:03:28 +0900

    Feat: basic toggle func for switching theme

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Fragment, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Reset } from 'styled-reset';
 import { ThemeProvider } from 'styled-components';
 import { Home } from './pages/index';
 import GlobalStyle from './globalStyles';
