3df19eb3dbe373e0e5b8a9b57335d9a493a13095
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-11 23:04:52 +0900

    Feat: RouterSetting & Home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Home } from './pages/index';
 
-function App() {
-  return <div>Hello</div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Route exact path="/" component={Home} />
+    </Router>
+  );
+};
 
 export default App;
