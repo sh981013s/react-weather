91cb9b5f668abe9c5ba58867f42504d8cad318e2
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-13 15:41:11 +0900

    Feat: added useFetch hook using axios

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useFetch = (url) => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [url]);
+
+  return { loading, data, error };
+};
+
+export default useFetch;
