a67904b1caa9ea777859470cdef479a2d89d5b54
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-14 21:52:28 +0900

    Design: 도시 svg import & 색감 테마별 미세 조정

--- src/components/home/Card.js
+++ src/components/home/Card.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { ReactComponent as DarkCity } from '../../assets/svgs/darkCity.svg';
+import { ReactComponent as LightCity } from '../../assets/svgs/lightCity.svg';
+import { useTheme } from '../../hooks/useTheme';
 
 const Box = styled.li`
   width: 16rem;
   height: 40vh;
-  background: ${(props) => props.theme.text};
-  color: ${(props) => props.theme.body};
+  background: ${(props) => props.theme.card};
+  color: ${(props) => props.theme.text};
   padding: 1.5rem 2rem;
   margin-right: 8rem;
-  border-radius: 0 50px 0 50px;
+  border-radius: 50px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   border: 1px solid ${(props) => props.theme.body};
   transition: all 0.2s ease;
+  box-shadow: 0 0 2rem rgb(0 0 255 / 60%);
 
   &:hover {
     background: ${(props) => props.theme.body};
@@ -23,7 +27,10 @@ const Box = styled.li`
 `;
 
 const Card = () => {
-  return <Box></Box>;
+  const { theme } = useTheme();
+  return (
+    <Box>{theme.themeName === 'lightTheme' ? <LightCity /> : <DarkCity />}</Box>
+  );
 };
 
 export default Card;
