82f628b1ab7bc06461455f2075cd348121028371
SeungHwan-Lee <sh981013s@gmail.com>
2022-01-20 15:47:35 +0900

    Feat: user data complete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import styled, { ThemeProvider } from 'styled-components';
 import Navbar from './components/general/Navbar/Navbar';
 import { Home, Login, SignUp, WeatherDetail, AddCity } from './pages/index';
@@ -7,7 +7,7 @@ import GlobalStyle from './globalStyles';
 import { LightTheme, DarkTheme } from './utility/Theme';
 import { useTheme } from './hooks/useTheme';
 import Loader from './components/general/Loader';
-import { auth, signInWithGoogle } from './firebase/firebaseConfig';
+import { useAuthContext } from './hooks/useAuthContext';
 
 const ContentBox = styled.div`
   width: 100vw;
@@ -19,35 +19,35 @@ const ContentBox = styled.div`
 
 const App = () => {
   const { theme } = useTheme();
-  useEffect(() => {
-    auth.onAuthStateChanged((user) => {
-      if (user !== null) {
-        console.log('logged in');
-        console.log(user);
-      } else {
-        console.log('no user');
-      }
-    });
-  }, []);
+  const { user, authIsReady } = useAuthContext();
+
   return (
     <>
       <GlobalStyle />
       <ThemeProvider
         theme={theme.themeName === 'lightTheme' ? LightTheme : DarkTheme}
       >
-        {/* <Loader /> */}
+        {authIsReady && (
+          <Router>
+            <Navbar />
 
-        <Router>
-          <Navbar />
-
-          <ContentBox>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignUp} />
-            <Route exact path="/addcity" component={AddCity} />
-            <Route path="/city/:name/:lat/:long" component={WeatherDetail} />
-          </ContentBox>
-        </Router>
+            <ContentBox>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route exact path="/login">
+                {user && <Redirect to="/" />}
+                <Login />
+              </Route>
+              <Route exact path="/signup">
+                {user && <Redirect to="/" />}
+                <SignUp />
+              </Route>
+              <Route exact path="/addcity" component={AddCity} />
+              <Route path="/city/:name/:lat/:long" component={WeatherDetail} />
+            </ContentBox>
+          </Router>
+        )}
       </ThemeProvider>
     </>
   );
