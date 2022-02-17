9d57346f0aa08059526f4a920f7558f6cdbb8b0f
SeungHwan-Lee <sh981013s@gmail.com>
2022-02-17 16:35:51 +0900

    feat: alert provider 추가

--- src/index.js
+++ src/index.js
@@ -3,14 +3,25 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
 import { AuthContextProvider } from './context/AuthContext';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-oldschool-dark';
+
+const options = {
+  position: positions.MIDDLE,
+  timeout: 5000,
+  offset: '30px',
+  transition: transitions.SCALE,
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </ThemeProvider>
+    <AlertProvider template={AlertTemplate} {...options}>
+      <ThemeProvider>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </ThemeProvider>
+    </AlertProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
